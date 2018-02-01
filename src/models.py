51f6be5985b40e61f3c720bbf6fd01df07638ddf
RenatoBMLR <renatobmlr@gmail.com>
2018-02-01 18:08:09 -0200

    create BoWClassifier

--- src/models.py
+++ src/models.py
@@ -13,6 +13,30 @@ import torch.nn.functional as F
 import torch.nn as nn
 
 
+class BoWClassifier(nn.Module):  # inheriting from nn.Module!
+
+    def __init__(self, num_labels, vocab_size):
+        # calls the init function of nn.Module.  Dont get confused by syntax,
+        # just always do it in an nn.Module
+        super(BoWClassifier, self).__init__()
+
+        # Define the parameters that you will need.  In this case, we need A and b,
+        # the parameters of the affine mapping.
+        # Torch defines nn.Linear(), which provides the affine map.
+        # Make sure you understand why the input dimension is vocab_size
+        # and the output is num_labels!
+        self.linear = nn.Linear(vocab_size, num_labels)
+
+        # NOTE! The non-linearity log softmax does not have parameters! So we don't need
+        # to worry about that here
+
+    def forward(self, bow_vec):
+        # Pass the input through the linear layer,
+        # then pass that through log_softmax.
+        # Many non-linearities and other functions are in torch.nn.functional
+        return F.log_softmax(self.linear(bow_vec), dim=1)
+    
+    
 class Net(nn.Module):
     def __init__(self, nb_input, nb_output):
         super(Net, self).__init__()
