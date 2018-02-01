34a9adb1e0069a0c32e709908a0f502079fa7351
RenatoBMLR <renatobmlr@gmail.com>
2018-02-01 18:16:29 -0200

    removed net from models file

--- src/models.py
+++ src/models.py
@@ -35,18 +35,4 @@ class BoWClassifier(nn.Module):  # inheriting from nn.Module!
         # then pass that through log_softmax.
         # Many non-linearities and other functions are in torch.nn.functional
         return F.log_softmax(self.linear(bow_vec), dim=1)
-    
-    
-class Net(nn.Module):
-    def __init__(self, nb_input, nb_output):
-        super(Net, self).__init__()
-        # Dense
-        self.fc1 = nn.Linear(nb_input, 128)
-        self.fc2 = nn.Linear(128, 32)
-        self.fc3 = nn.Linear(32, nb_output)
-        
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return F.log_softmax(x)
+
