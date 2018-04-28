ea3e1bdc4f9dded1014ce0e1f9c59af710f4a2c7
Renato Rodrigues <renatobmlr@gmail.com>
2018-04-28 09:12:05 -0300

    clean code

--- src/textDataset.py
+++ src/textDataset.py
@@ -22,7 +22,7 @@ from torch.utils.data import Dataset, DataLoader
 def create_dataLoader(dsets, batch_size,  pin_memory=False, use_shuffle=False):
 
     dset_loaders = {}
-   
+
     shuffle = False
     for key in dsets.keys():
         if use_shuffle:
@@ -31,38 +31,35 @@ def create_dataLoader(dsets, batch_size,  pin_memory=False, use_shuffle=False):
             else:
                 shuffle = False
         dset_loaders[key] = DataLoader(dsets[key], batch_size=batch_size, pin_memory=pin_memory, shuffle = shuffle)
-            
+
     return dset_loaders
 
 
-class TextDataset(Dataset):
-    
-    
+class TextTorchDataset(Dataset):
+
     def __init__(self, x, y, is_test = False, transform=None):
-    
         self.x = x
         if is_test:
             self.y = np.zeros(len(self.x))
         else:
             self.y = y
         self.transform = transform
-    
-    
+
     def __len__(self):
         return len(self.x)
 
     def __getitem__(self, idx):
-        
+
         x = self.x[idx]
         y = self.y[idx]
 
         if self.transform is not None:
             x = self.transform(x)
-        
+
         return x, y
 
 
-class TextCleaner():
+class TextDataset():
 
     def __init__(self, root_dir, val_size=0.1,
                  is_valid=False, is_test=False, is_train = False, lang = 'english'):
@@ -74,25 +71,24 @@ class TextCleaner():
         """
         self.data = pd.DataFrame()
         self.root_dir = root_dir
-        
-        self.tokenizer = TweetTokenizer() 
+
+        self.tokenizer = TweetTokenizer()
         self.stop_words = set(stopwords.words(lang))
         self.lemmatizer = WordNetLemmatizer()
         self.ps = PorterStemmer()
 
         self._read_data()
         #self.data = (self.data if len(col_lst) else self.data[col_lst])
-        
+
         if ( (is_train) or (is_valid)):
             self.y = self.data[self.data.columns[:-1]].values
 
             if is_valid:
                 self.split_data(val_size)
-                
+
         elif is_test:
             self.y = np.zeros([len(self.data), len(self.data.columns[:-1])])
-          
-        
+
     def split_data(self, val_size = 0.1):
 
         np.random.seed(4572)
@@ -101,9 +97,9 @@ class TextCleaner():
         ind = np.random.permutation(indices)
         split = np.round(val_size * len(self.data))
         index= np.array(ind[:int(split)])
-        
+
         self.data = self.data.take(index,axis=0)
-        self.y = self.y.take(index,axis=0)      
+        self.y = self.y.take(index,axis=0)
 
     def _removeStopwords(self, words):
         # Removing all the stopwords
@@ -114,19 +110,16 @@ class TextCleaner():
         words = words.lower()
         return re.sub(r'[^\w\s]', '', words)
 
-    
     def _lemmatizing(self, words):
         #Lemmatizing
         return ' '.join(self.lemmatizer.lemmatize(word) for word in words.split())
 
-    
     def _stemming(self, words):
         #Stemming
         return ' '.join(self.ps(word) for word in words.split())
-    
-        
+
     def _removeTagsAndUris(self, x):
-    
+
         uri_re = r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))'
         # BeautifulSoup on content
         soup = BeautifulSoup(x, "html.parser")
@@ -139,36 +132,22 @@ class TextCleaner():
         return re.sub(uri_re, "", text)
 
     def process_data(self, col = 'content', remove_pontuation = True,  remove_stopw = False, remove_tags = False, lemmalize = False, stem = False):
-        
+
         if remove_pontuation:
             self.data[col + '_data'] = self.data[col].apply(lambda x: self._removePonctuation(x) )
-        
         if remove_stopw:
             self.data[col + '_data'] = self.data[col + '_data'].apply(lambda x: self._removeStopwords(x)) 
-        
         if remove_tags:
             self.data[col + '_data'] = self.data[col + '_data'].apply(lambda x: self._removeTagsAndUris(x) )
-        
         if lemmalize:
             self.data[col + '_data'] = self.data[col + '_data'].apply(lambda x: self._lemmatizing(x) )
-        
+
         if stem:
             self.data[col + '_data'] = self.data[col + '_data'].apply(lambda x: self._stemming(x))
 
-        self.data[col + '_data'] = self.data[col + '_data'].apply(lambda x: ' '.join(set(x.split())))
+        #self.data[col + '_data'] = self.data[col + '_data'].apply(lambda x: ' '.join(set(x.split())))
+        self.data[col + '_data'] = self.data[col + '_data'].apply(lambda x: set(x.split()))
 
-    def make_bow_vector(self, col, words_ix):
-        
-        bow_vector = []
-        for i, sentence in self.data[col].iteritems():
-        
-            vec = torch.zeros(len(words_ix))
-            for word in sentence.split():
-                vec[words_ix[word]] += 1
-            bow_vector.append(vec.view(1, -1))
-        return bow_vector
-        
-    
     def _read_data(self):
 
         for root, dirs, files in os.walk(self.root_dir):
@@ -177,25 +156,30 @@ class TextCleaner():
                 if file_extension == '.csv':
                     df_aux = pd.read_csv(os.path.join(root, file))
                     sub = file.split('/')[-1][:-4]
-                    df_aux['subject'] = sub    
+                    df_aux['subject'] = sub
                     self.data=self.data.append(df_aux)
 
+    def make_bow_vector(self, col, words_ix):
+
+        bow_vector = []
+        for i, sentence in self.data[col].iteritems():
+
+            vec = torch.zeros(len(words_ix))
+            for word in sentence.split():
+                vec[words_ix[word]] += 1
+            bow_vector.append(vec.view(1, -1))
+        return bow_vector
+
     def get_data(self, x_col, words_ix, y_col = []):
-        
+
         x = self.make_bow_vector(x_col, words_ix)
         y=self.data[y_col]
-        
         return (x,y)
-            
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        
         textSample = self.data['data'].iloc[idx]
         y = self.data['subject'].iloc[idx]
-        
+
         return textSample, y
-
-
- 
